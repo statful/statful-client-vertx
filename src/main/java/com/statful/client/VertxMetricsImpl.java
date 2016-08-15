f2ddc591784c3c000b4708c24a1426269254bd8c
Tiago Reis <tiago.reis@mindera.com>
2016-08-15 13:22:00 +0100

    fix(error-creating-collectors): stop storing all collectors.
    Storing all collectors was making the collectors list grow too much. We only need to store the pool collectors since that is the one that we are unable to create the sender in time

--- src/main/java/com/statful/client/VertxMetricsImpl.java
+++ src/main/java/com/statful/client/VertxMetricsImpl.java
@@ -1,6 +1,5 @@
 package com.statful.client;
 
-import com.google.common.collect.Lists;
 import com.statful.collector.*;
 import com.statful.sender.Sender;
 import com.statful.sender.SenderFactory;
@@ -16,7 +15,7 @@ import io.vertx.core.spi.metrics.HttpClientMetrics;
 import io.vertx.core.spi.metrics.HttpServerMetrics;
 import io.vertx.core.spi.metrics.PoolMetrics;
 
-import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * VertxMetrics SPI implementation for statful metrics collection
@@ -40,9 +39,10 @@ final class VertxMetricsImpl extends DummyVertxMetrics {
     private final Vertx vertx;
 
     /**
-     * Holds all collectors created to lazy load the client
+     * Holds all poolMetricsCollectors created to lazy load the client.
+     * Since metrics can be created by different eventloop threads or worker threads we need handle concurrency here
      */
-    private final List<StatfulMetrics> collectors;
+    private final LinkedBlockingQueue<StatfulMetrics> poolMetricsCollectors;
 
     /**
      * Constructor to be used for configuration and creation of a sender
@@ -53,24 +53,23 @@ final class VertxMetricsImpl extends DummyVertxMetrics {
     VertxMetricsImpl(final Vertx vertx, final StatfulMetricsOptions statfulMetricsOptions) {
         this.vertx = vertx;
         this.statfulMetricsOptions = statfulMetricsOptions;
-        this.collectors = Lists.newLinkedList();
+        this.poolMetricsCollectors = new LinkedBlockingQueue<>();
     }
 
     @Override
     public HttpClientMetrics<HttpRequestMetrics, SocketAddress, SocketAddress, Void, Void> createMetrics(final HttpClient client,
                                                                                                          final HttpClientOptions options) {
         HttpClientMetricsImpl httpClientMetricsImpl = new HttpClientMetricsImpl(statfulMetricsOptions);
-        this.collectors.add(httpClientMetricsImpl);
-        this.collectors.forEach(collector -> collector.setSender(this.getOrCreateSender()));
+        httpClientMetricsImpl.setSender(this.getOrCreateSender());
+        this.poolMetricsCollectors.forEach(collector -> collector.setSender(this.getOrCreateSender()));
         return httpClientMetricsImpl;
     }
 
     @Override
     public HttpServerMetrics createMetrics(final HttpServer server, final SocketAddress localAddress, final HttpServerOptions options) {
         HttpServerMetricsImpl httpServerMetricsImpl = new HttpServerMetricsImpl(statfulMetricsOptions);
-        this.collectors.add(httpServerMetricsImpl);
-        this.collectors.forEach(collector -> collector.setSender(this.getOrCreateSender()));
-
+        httpServerMetricsImpl.setSender(this.getOrCreateSender());
+        this.poolMetricsCollectors.forEach(collector -> collector.setSender(this.getOrCreateSender()));
         return httpServerMetricsImpl;
     }
 
@@ -78,9 +77,9 @@ final class VertxMetricsImpl extends DummyVertxMetrics {
     public <P> PoolMetrics<?> createMetrics(final P pool, final String poolType, final String poolName, final int maxPoolSize) {
 
         // When creating pool metrics we cannot create the sender because vertx is not completely initialized
-        // This is an hack and the reporter will need to be decoupled from the collectors
+        // This is an hack and the reporter will need to be decoupled from the poolMetricsCollectors
         PoolMetricsImpl poolMetrics = new PoolMetricsImpl(statfulMetricsOptions, vertx, poolType, poolName, maxPoolSize);
-        this.collectors.add(poolMetrics);
+        this.poolMetricsCollectors.add(poolMetrics);
         return poolMetrics;
     }
 
