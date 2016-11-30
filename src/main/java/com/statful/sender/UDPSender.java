788d78dfcdf3bf185626a7d4f7d7c0837f82f686
Tiago Reis <tiago.reis@mindera.com>
2016-11-30 14:14:48 +0000

    fix(NA): Improve error log on failed metrics

--- src/main/java/com/statful/sender/UDPSender.java
+++ src/main/java/com/statful/sender/UDPSender.java
@@ -74,7 +74,7 @@ public final class UDPSender extends MetricsHolder {
 
         socket.send(toSendMetrics, options.getPort(), options.getHost(), handler -> {
             if (handler.failed()) {
-                LOGGER.warn("Failed to send metrics {}", toSendMetrics);
+                LOGGER.error("Failed to send metrics {}", handler.cause(),  toSendMetrics);
                 endHandler.ifPresent(callerHandler -> callerHandler.handle(Future.failedFuture(handler.cause())));
             } else {
                 endHandler.ifPresent(callerHandler -> callerHandler.handle(Future.succeededFuture()));
